f437e7f3215c3980d76991cc942d5a69f3e770aa
admin <admin@DESKTOP-697FU78>
2025-11-30 13:27:38 +0200

    Changed avatar and info

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,8 +6,8 @@ const Body = () => {
       <div style={styles.card}>
         <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Kishko Mykhailo</h2>
+          <p style={styles.position}>Business analyst, Dev-Ops engineer</p>
         </div>
       </div>
     </main>
